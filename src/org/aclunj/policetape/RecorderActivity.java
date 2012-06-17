be8647467bf5bd91124b5c33f5caf2b89103d5c9
David Brodsky <davidpbrodsky@gmail.com>
2012-06-17 15:10:51 -0700

    Toogle camcorder container visibility instead of camcorder preview. Avoids misbehavior in froyo

--- src/org/aclunj/policetape/RecorderActivity.java
+++ src/org/aclunj/policetape/RecorderActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class RecorderActivity extends Activity {
     /** Called when the activity is first created. */
@@ -22,6 +23,7 @@ public class RecorderActivity extends Activity {
     final Handler mHandler = new Handler();
     public VideoRecorder vr;
     //private ImageView iv;
+    private LinearLayout container;
     private FrameLayout fl;
     private MainActivityGroup mag;
     private Activity mainer;
@@ -35,6 +37,7 @@ public class RecorderActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.playa);
+        container = (LinearLayout) findViewById(R.id.camcorder_container);
         
         vr = (VideoRecorder) findViewById(R.id.camcorder_preview);
         //iv = (ImageView) findViewById(R.id.hider);
@@ -64,7 +67,7 @@ public class RecorderActivity extends Activity {
        
     
     public void start() {
-            vr.setVisibility(View.VISIBLE);
+            container.setVisibility(View.VISIBLE);
             //iv.setVisibility(View.VISIBLE);
             hidden = true;
             
@@ -93,7 +96,7 @@ public class RecorderActivity extends Activity {
                     try {
                         recording = false;
                         vvv.stop();
-                        vr.setVisibility(View.GONE);
+                        container.setVisibility(View.GONE);
                         //iv.setVisibility(View.GONE);
                         hidden = false;
                     } catch (IOException e) {
